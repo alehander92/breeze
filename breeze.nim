be944445eec6ba55d0162cef3ba02491920e218a
planetis-m <b3lievem@yandex.com>
2020-11-14 14:13:54 +0200

    compiles

--- breeze.nim
+++ breeze.nim
@@ -1,6 +1,4 @@
-import macros, strutils, sequtils, tables
-
-const debugMacro = false
+import macros, strutils, sequtils
 
 proc build(b: NimNode): NimNode
 
@@ -16,25 +14,15 @@ proc buildInline(args: NimNode): seq[NimNode] =
     else:
       isArg = true
 
-proc labelOf(b: NimNode): string =
-  assert b.kind in {nnkIdent, nnkAccQuoted}
-  if b.kind == nnkAccQuoted:
-    result = labelOf(b[0])
-  else:
-    result = $b.ident
-
 proc buildIdent(b: NimNode): NimNode =
   result = nnkCall.newTree(
-    newIdentNode(!"newIdentNode"),
-    nnkPrefix.newTree(
-      newIdentNode(!"!"),
-      b))
+    newIdentNode("newIdentNode"), b)
 
 proc build(b: NimNode): NimNode =
-  var resultNode = newIdentNode(!"result")
-  var stmtsNode = newIdentNode(!"stmts")
-  var lastNode = newIdentNode(!"last")
-  var x = newIdentNode(!"x")
+  var resultNode = newIdentNode("result")
+  var stmtsNode = newIdentNode("stmts")
+  var lastNode = newIdentNode("last")
+  var x = newIdentNode("x")
   case b.kind:
   of nnkStmtList:
     result = nnkStmtList.newTree()
@@ -43,9 +31,8 @@ proc build(b: NimNode): NimNode =
   of nnkVarSection:
     result = b
   of nnkCall:
-    var label = labelOf(b[0])
-    case label:
-    of "ident":
+    var label = name(b[0])
+    if label.eqIdent"ident":
       result = buildIdent(newLit($b[1]))
       result = quote:
         `lastNode`.add(`result`)
@@ -55,11 +42,11 @@ proc build(b: NimNode): NimNode =
         args = buildCall(b[1])
       else:
         args = buildInline(b)
-      var callIdent = newIdentNode(!("nnk$1" % capitalizeAscii(label)))
+      var callIdent = newIdentNode("nnk" & $label)
       result = nnkCall.newTree(
         nnkDotExpr.newTree(
           callIdent,
-          newIdentNode(!"newTree")))
+          newIdentNode("newTree")))
       result = quote:
         `x` = `result`
         `lastNode`.add(`x`)
@@ -72,14 +59,14 @@ proc build(b: NimNode): NimNode =
       result.add(b)
   of nnkCharLit..nnkUInt64Lit, nnkFloatLit..nnkFloat64Lit, nnkStrLit..nnkTripleStrLit:
     result = nnkCall.newTree(
-      newIdentNode(!"newLit"),
+      newIdentNode("newLit"),
       b)
     result = quote:
       `lastNode`.add(`result`)
   of nnkIdent:
     if $b == "true" or $b == "false":
       result = nnkCall.newTree(
-        newIdentNode(!"newLit"),
+        newIdentNode("newLit"),
         b)
     else:
       result = b
@@ -103,7 +90,7 @@ proc build(b: NimNode): NimNode =
           build(branch[0])))
   of nnkInfix:
     if b[1].kind == nnkStrLit:
-      var newLitNode = newIdentNode(!"newLit")
+      var newLitNode = newIdentNode("newLit")
       result = quote:
         `newLitNode`(`b`)
     else:
@@ -122,27 +109,25 @@ proc build(b: NimNode): NimNode =
   else:
     result = quote:
       `lastNode`.add(`b`)
-    when debugMacro:
-      echo treerepr(b)
-    else:
-      discard
+    when defined(debugBreeze):
+      echo treeRepr(b)
 
 macro buildMacro*(b: untyped): untyped =
-  var stmtsNode = newIdentNode(!"stmts")
-  var lastNode = newIdentNode(!"last")
-  var x = newIdentNode(!"x")
-  var resultNode = newIdentNode(!"result")
+  var stmtsNode = newIdentNode("stmts")
+  var lastNode = newIdentNode("last")
+  var x = newIdentNode("x")
+  var resultNode = newIdentNode("result")
   var start = quote:
     var `stmtsNode` = nnkStmtList.newTree()
     var `lastNode` = `stmtsNode`
     var `x`: NimNode
   var finish = quote:
     `resultNode` = `stmtsNode`
-  
+
   start.add(build(b))
   start.add(finish)
   var empty = newEmptyNode()
-  
+
   result = nnkCall.newTree(
     nnkPar.newTree(
       nnkLambda.newTree(
@@ -150,11 +135,28 @@ macro buildMacro*(b: untyped): untyped =
         empty,
         empty,
         nnkFormalParams.newTree(
-          newIdentNode(!"NimNode")),
+          newIdentNode("NimNode")),
         empty,
         empty,
         start)))
 
-  when debugMacro:
-    echo "build: $1" % repr(result)
+  when defined(debugBreeze):
+    echo "build:\n", repr(result)
 
+when isMainModule:
+  macro cases(state, event, table: untyped): untyped =
+    expectKind(table[0], nnkBracket)
+    result = buildMacro:
+      caseStmt(state):
+        for n in table[0]:
+          expectKind(n, nnkExprColonExpr)
+          expectKind(n[1], nnkBracket)
+          expectMinLen(n[1], 1)
+          ofBranch(n[0]):
+            ifStmt:
+              for m in n[1]:
+                expectKind(m, nnkExprColonExpr)
+                elifBranch(call(ident"==", m[0], event)):
+                  asgn(state, call(m[1]))
+              `else`:
+                discardStmt(empty)
